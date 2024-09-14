aec12f384224cb4475d115b151a5c9abf8abd859
Simon Walenkamp Hansen <swh@relewise.com>
2024-09-14 17:07:04 +0200

    Try adding production routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/walenkamp/',
+  base: isProd ? '/walenkamp/' : undefined,  
   plugins: [vue()],
 })
