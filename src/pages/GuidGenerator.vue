4cbb9fd6694a540136239d838080056dd021eda7
Simon Walenkamp Hansen <swh@relewise.com>
2024-10-10 20:59:38 +0200

    Add a copy btn to guid generator

--- src/pages/GuidGenerator.vue
+++ src/pages/GuidGenerator.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-    const guid = ref(crypto.randomUUID());
+const guid = ref(crypto.randomUUID());
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(guid.value.toString())
+}
 </script>
 
 <template>
+<div class="flex gap-2">
     <h1>{{ guid }}</h1>
+    <button @click="copyToClipboard">Copy</button>
+</div>    
 </template>
