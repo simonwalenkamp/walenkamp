36f6263127dd259d9b86245875caaad7773b345b
Simon Walenkamp <simonwalenkamp@hotmail.com>
2025-04-20 00:27:19 +0200

    Remove router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
@@ -10,6 +9,5 @@ import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 library.add(faLinkedin, faSquareGithub, faEnvelopeSquare)
 
 createApp(App)
-  .use(router)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
